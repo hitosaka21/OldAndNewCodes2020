2c423ce48a01b36b5fa75962ef0552f3d6cf44fc
hitosaka21 <osaka708@gmail.com>
2020-12-08 00:41:26 +0900

    create ModernActivity

---
+++ app/src/main/java/com/example/oldandnewcodes2020/modern/ModernActivity.kt
@@ -0,0 +1,12 @@
+package com.example.oldandnewcodes2020.modern
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.oldandnewcodes2020.R
+
+class ModernActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modern)
+    }
+}
