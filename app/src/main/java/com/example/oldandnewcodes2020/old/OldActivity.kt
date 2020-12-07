02b3f4ecf630616da6a9ae3dac8324faf80e1e1c
hitosaka21 <osaka708@gmail.com>
2020-12-08 02:12:01 +0900

    show OldMainFragment

--- app/src/main/java/com/example/oldandnewcodes2020/old/OldActivity.kt
+++ app/src/main/java/com/example/oldandnewcodes2020/old/OldActivity.kt
@@ -1,12 +1,16 @@
 package com.example.oldandnewcodes2020.old
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.oldandnewcodes2020.R
 
 class OldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_old)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.old_main_container, OldMainFragment())
+        transaction.commit()
     }
 }
