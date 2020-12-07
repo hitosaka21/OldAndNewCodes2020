e0b5e7a50bf6071c59f2e6b4731a09dfb3199cc7
hitosaka21 <osaka708@gmail.com>
2020-12-08 00:29:37 +0900

    go to OldActivity

--- app/src/main/java/com/example/oldandnewcodes2020/MainFragment.kt
+++ app/src/main/java/com/example/oldandnewcodes2020/MainFragment.kt
@@ -1,17 +1,36 @@
 package com.example.oldandnewcodes2020
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.oldandnewcodes2020.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_oldActivity)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
