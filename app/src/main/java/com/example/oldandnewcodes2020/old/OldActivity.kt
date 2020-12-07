7ac95d6127d195470983eb2feb7ff6906266efdd
hitosaka21 <osaka708@gmail.com>
2020-12-07 23:34:50 +0900

    create OldActivity

---
+++ app/src/main/java/com/example/oldandnewcodes2020/old/OldActivity.kt
@@ -0,0 +1,12 @@
+package com.example.oldandnewcodes2020.old
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.oldandnewcodes2020.R
+
+class OldActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_old)
+    }
+}
