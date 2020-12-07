86f2b899ea49a71861a7ead76b08531e857eb296
hitosaka21 <osaka708@gmail.com>
2020-12-08 02:34:39 +0900

    use Kotlin Android Extensions

--- app/src/main/java/com/example/oldandnewcodes2020/old/OldMainFragment.kt
+++ app/src/main/java/com/example/oldandnewcodes2020/old/OldMainFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.oldandnewcodes2020.R
+import kotlinx.android.synthetic.main.fragment_old_main.*
 
 class OldMainFragment : Fragment() {
 
@@ -15,5 +17,13 @@ class OldMainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_old_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        old_main_button.setOnClickListener {
+            Toast.makeText(context, "SHOW TOAST", Toast.LENGTH_SHORT).show()
+        }
     }
 }
