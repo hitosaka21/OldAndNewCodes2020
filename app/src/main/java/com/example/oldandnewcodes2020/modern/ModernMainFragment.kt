557a31d1b92fab89d40933e1f2d7cb8ba56a2ee7
hitosaka21 <osaka708@gmail.com>
2020-12-09 22:08:16 +0900

    compare onActivityCreated : initialization sample

--- app/src/main/java/com/example/oldandnewcodes2020/modern/ModernMainFragment.kt
+++ app/src/main/java/com/example/oldandnewcodes2020/modern/ModernMainFragment.kt
@@ -1,17 +1,30 @@
 package com.example.oldandnewcodes2020.modern
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.oldandnewcodes2020.R
 import com.example.oldandnewcodes2020.databinding.FragmentModernMainBinding
 
 class ModernMainFragment : Fragment() {
 
     private var _binding: FragmentModernMainBinding? = null
     private val binding get() = _binding!!
+
+    private var message: String? = null
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        message = savedInstanceState?.getString("KEY") ?: "not save"
+        Log.d(this::class.simpleName, "message=$message")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,9 +37,17 @@ class ModernMainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        textView = view.findViewById(R.id.modern_main_text_view)
+
         binding.modernMainButton.setOnClickListener {
             Toast.makeText(context, "SHOW TOAST", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("KEY", "save")
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/oldandnewcodes2020/old/OldMainFragment.kt
+++ app/src/main/java/com/example/oldandnewcodes2020/old/OldMainFragment.kt
@@ -1,15 +1,19 @@
 package com.example.oldandnewcodes2020.old
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.oldandnewcodes2020.R
 import kotlinx.android.synthetic.main.fragment_old_main.*
 
 class OldMainFragment : Fragment() {
+    private var message: String? = null
+    private var textView: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,8 +26,19 @@ class OldMainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        message = savedInstanceState?.getString("KEY") ?: "not save"
+        Log.d(this::class.simpleName, "message=$message")
+
+        textView = view?.findViewById(R.id.old_main_text_view)
+
         old_main_button.setOnClickListener {
             Toast.makeText(context, "SHOW TOAST", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("KEY", "save")
     }
 }
